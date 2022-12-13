d6f504dc9a3857b9e1e275703802ca575482e979
KenMR <kmr5591@gmail.com>
2022-12-12 19:59:03 -0500

    Add basic logic to claim spaces and call the array they're stored in outside the gameboard object

--- odin-tic-tac-toe.js
+++ odin-tic-tac-toe.js
@@ -10,7 +10,22 @@ const player = (name) => {
     }
     playerNum++;
     return {name, choice};
-  };
+};
+
+const gameboard = (() => {
+    const boardArray = [];
+
+    const getBoard = (space) => {
+        return boardArray[space];
+    };
+
+    const claimSpace = (space, player) => {
+        boardArray[space] = player.choice;
+        // update css to display the claimed space
+    };
+
+    return {boardArray, getBoard, claimSpace};
+})();
 
 let name1 = prompt("Please enter player one's name.");
   
@@ -23,3 +38,23 @@ const player2 = player(`${name2}`);
 console.log(`Player one's name is ${player1.name}. They are ${player1.choice}.`);
 
 console.log(`Player two's name is ${player2.name}. They are ${player2.choice}.`);
+
+// logic below tests the the space claim and boardArray can be checked outside of the gameboard object
+
+let claim = 0;
+
+gameboard.claimSpace(claim, player1);
+
+console.log(`${player1.name} claims space ${claim + 1} with ${gameboard.boardArray[claim]}`);
+
+claim++;
+
+gameboard.claimSpace(claim, player2);
+
+console.log(`${player2.name} claims space ${claim + 1} with ${gameboard.boardArray[claim]}`);
+
+claim++;
+
+gameboard.claimSpace(claim, player1);
+
+console.log(`${player1.name} claims space ${claim + 1} with ${gameboard.boardArray[claim]}`);
