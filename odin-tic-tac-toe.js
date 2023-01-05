f617d028e8a8b13188aa1c64b1dcffbbe5da8b2f
KenMR <kmr5591@gmail.com>
2023-01-04 20:17:16 -0500

    Create gamelogic module and basic testing function

--- odin-tic-tac-toe.js
+++ odin-tic-tac-toe.js
@@ -1,6 +1,8 @@
 let playerNum = 0;
+let claim = 0;
 
 const player = (name) => {
+
     let choice;
     if (playerNum === 0) {
         choice = 'X';
@@ -13,6 +15,7 @@ const player = (name) => {
 };
 
 const gameboard = (() => {
+
     const boardArray = [];
 
     for (i = 0; i < 9; i++) {
@@ -35,34 +38,46 @@ const gameboard = (() => {
     return {boardArray, claimSpace};
 })();
 
-let name1 = prompt("Please enter player one's name.");
-  
-const player1 = player(`${name1}`);
+const gamelogic = (() => {
 
-let name2 = prompt("Please enter player two's name.");
-  
-const player2 = player(`${name2}`);
+    const player1 = player(`${prompt("Please enter player one's name.")}`);
 
-console.log(`Player one's name is ${player1.name}. They are ${player1.choice}.`);
+    const player2 = player(`${prompt("Please enter player two's name.")}`);
 
-console.log(`Player two's name is ${player2.name}. They are ${player2.choice}.`);
+    console.log(`Player one's name is ${player1.name}. They are ${player1.choice}.`);
 
-// logic below tests the the space claim and boardArray can be checked outside of the gameboard object
+    console.log(`Player two's name is ${player2.name}. They are ${player2.choice}.`);
 
-let claim = 0;
+    const claimTest = () => {
+        // logic below tests the the space claim and boardArray can be checked outside of the gameboard object
 
-gameboard.claimSpace(claim, player1);
+        gameboard.claimSpace(claim, player1);
 
-console.log(`${player1.name} claims space ${claim + 1} with ${gameboard.boardArray[claim]}`);
+        console.log(`${player1.name} claims space ${claim + 1} with ${gameboard.boardArray[claim]}`);
 
-claim++;
+        claim++;
 
-gameboard.claimSpace(claim, player2);
+        gameboard.claimSpace(claim, player2);
 
-console.log(`${player2.name} claims space ${claim + 1} with ${gameboard.boardArray[claim]}`);
+        console.log(`${player2.name} claims space ${claim + 1} with ${gameboard.boardArray[claim]}`);
 
-claim++;
+        claim++;
 
-gameboard.claimSpace(claim, player1);
+        gameboard.claimSpace(claim, player1);
 
-console.log(`${player1.name} claims space ${claim + 1} with ${gameboard.boardArray[claim]}`);
+        console.log(`${player1.name} claims space ${claim + 1} with ${gameboard.boardArray[claim]}`);
+    };
+
+    const winStates = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+
+    return {claimTest, winStates, player1, player2};
+})();
